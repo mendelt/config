2105e3a8c5854ff7941846dd40aeb5312964ce26
Mendelt Siebenga <mendelt@mendelt.nl>
2022-09-01 21:54:40 +0200

    nvim: add nvim-tree plugin

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -5,5 +5,8 @@ return require('packer').startup(function(use)
     use "savq/melange"
     use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
 
+    -- File explorer
+    use { 'kyazdani42/nvim-tree.lua', requires = { 'kyazdani42/nvim-web-devicons' } }
+    require("nvim-tree").setup()
 end)
 
