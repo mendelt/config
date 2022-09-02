35b881eeb4a1a12452d47f00926c9d151bccae1b
Mendelt Siebenga <mendelt@mendelt.nl>
2022-09-02 20:41:14 +0200

    nvim: move plugin config out of plugins.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,3 +3,7 @@ require "user.keymaps"
 require "user.plugins"
 require "user.colorscheme"
 
+-- Per plugin config
+require "user.nvim-tree"
+require "user.treesitter"
+
--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -7,6 +7,5 @@ return require('packer').startup(function(use)
 
     -- File explorer
     use { 'kyazdani42/nvim-tree.lua', requires = { 'kyazdani42/nvim-web-devicons' } }
-    require("nvim-tree").setup()
 end)
 
