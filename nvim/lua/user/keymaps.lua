9ad2125b676210bf6c5ac12a9ddffa5d11254b76
Mendelt Siebenga <mendelt@mendelt.nl>
2022-09-09 14:09:59 +0200

    lua: keymap for tree

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -1,8 +1,13 @@
 local keymap = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+local term_opts = { silent = true }
 
 -- Window navigation
 -- keymap("n", "<C-h>", "<C-w>h, opts)
 -- keymap("n", "<C-j>", "<C-w>j, opts)
 -- keymap("n", "<C-k>", "<C-w>k, opts)
 -- keymap("n", "<C-l>", "<C-w>l, opts)
+
+-- Nvim tree
+keymap("n", "<leader>e", ":NvimTreeToggle<cr>", opts)
 
