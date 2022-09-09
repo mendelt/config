c9306a14e706d39f8d2e0493620aaa9dd6776884
Mendelt Siebenga <mendelt@mendelt.nl>
2022-09-09 14:09:48 +0200

    nvim: some completion stuff

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,6 +4,7 @@ require "user.plugins"
 require "user.colorscheme"
 
 -- Per plugin config
+require "user.cmp"
 require "user.lsp"
 require "user.nvim-tree"
 require "user.treesitter"
--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -13,6 +13,20 @@ return require('packer').startup(function(use)
     -- File explorer
     use { 'kyazdani42/nvim-tree.lua', requires = { 'kyazdani42/nvim-web-devicons' } }
 
+    -- cmp plugins
+    use 'hrsh7th/nvim-cmp'
+    use 'hrsh7th/cmp-nvim-lsp'
+
+    use 'hrsh7th/cmp-nvim-lua'
+    use 'hrsh7th/cmp-nvim-lsp-signature-help'
+    use 'hrsh7th/cmp-vsnip'
+    use 'hrsh7th/cmp-path'
+    use 'hrsh7th/cmp-buffer'
+    use 'hrsh7th/vim-vsnip'
+
+    -- snippets
+    use({ "rafamadriz/friendly-snippets", commit = "d27a83a363e61009278b6598703a763ce9c8e617" }) -- a bunch of snippets to use   
+
     -- toggleterm
     use({ "akinsho/toggleterm.nvim", commit = "aaeed9e02167c5e8f00f25156895a6fd95403af8" })
 end)
